e5af53ee53988f73d2bbb39b602a3bee1e41999e
damatgre <gregdamato2@gmail.com>
2021-07-12 20:35:42 -0400

    product model

--- models/Product.js
+++ models/Product.js
@@ -36,7 +36,16 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: category,
+        key: 'id'
+      }
     }
   },
   {
